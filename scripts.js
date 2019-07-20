33ed234c4f4a22b53d046c7d45ffd2251f5308ed
Patrick Ly <patrickkimly@gmail.com>
2019-07-20 11:46:36 -0700

    testing out error message

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-/* var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://ghibliapi.herokuapp.com/filmsz', true);
 
 request.onload = function () {
     //Begin accessing JSON data here
@@ -12,14 +12,39 @@ request.onload = function () {
     if(request.status >= 200 && request.status < 400){
         data.forEach(movie => {
             // log each movie's title
-            console.log(movie.title);
+            //console.log(movie.title);
+            //console.log(movie.description);
+            //console.log(movie.description);
+
+            const card = document.createElement('div');
+            card.setAttribute('class','card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+            //console.log(h1);
+
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...z`; // added a temporary z character 
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+            // template literals
+
+            container.appendChild(card);
+
+            card.appendChild(h1);
+            card.appendChild(p);
+
+
         });
     } else {
-        console.log('error');
+        //console.log('error');
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
     }
 }
 
-request.send(); */
+request.send();
 
 const app = document.getElementById('root');
 console.log("app: " + app); // concatenation
